8bb9e106394110accbd6111af43e7f7626198066
Andre Hickmann <andre.hickmann@swepp.eu>
2016-03-24 14:58:42 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# higg:orm
+
+## Table of contents
+
+- [Install](#install)
+- [Introduction](#install)
+- [Using](#using)
+- [Contributors](#contributors)
+- [Todos](#todos)
+
+
+## Install
+
+Adding the package to your meteor-application.
+
+ ```sh
+ $ meteor add higg:orm
+ ```
+
+## Introduction
+
+A basic orm system for your meteor (server) application.
+
+## Contributers
+
+If you like to contribute to the package, feel free to fork the repository and
+make a pull-request.
+
+## Todos
+
+Of course the package is very simple at the moment, there are plenty things to
+do:
+
+* writing tests :)
---
+++ package.js
@@ -0,0 +1,36 @@
+/* global Meteor, Package */
+Package.describe({
+  name: 'higg:orm',
+  version: '0.0.1',
+  summary: 'orm for mapping data from database-tables to models',
+  git: '',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('underscore', 'server');
+  api.use('ecmascript', 'server');
+  api.use('pcel:mysql', 'server');
+  api.use('higg:cache', 'server');
+  
+  api.addFiles('lib/orm.js', 'server');
+  
+  api.addFiles('lib/db.js', 'server');
+  api.addFiles('lib/db/adapter.js', 'server');
+  api.addFiles('lib/db/adapter/livemysql.js', 'server');
+  api.addFiles('lib/db/adapter/mysql.js', 'server');
+  api.addFiles('lib/db/adapter/select.js', 'server');
+  api.addFiles('lib/db/adapter/mysql/select.js', 'server');
+  api.addFiles('lib/db/table.js', 'server');
+  api.addFiles('lib/model.js');
+  
+  api.export('Orm');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('higg:orm');
+  api.addFiles('tests/orm-tests.js');
+});
